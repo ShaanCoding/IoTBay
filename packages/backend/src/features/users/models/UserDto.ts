a596b156a74357868a6fdd9338c633ee6ffca80c
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-04-07 20:29:40 +1000

    dob

--- packages/backend/src/features/users/models/UserDto.ts
+++ packages/backend/src/features/users/models/UserDto.ts
@@ -3,10 +3,24 @@ import { Static, Type } from "@sinclair/typebox";
 export const UserDto = Type.Object({
   userId: Type.String({ format: "uuid" }),
   email: Type.String({ format: "email" }),
+  name: Type.String(),
   userType: Type.String({ enum: ["staff", "customer"] }),
   shippingAddress: Type.Optional(Type.String()),
   billingAddress: Type.Optional(Type.String()),
-  name: Type.String()
+  dob: Type.Optional(Type.String({ format: "date" })),
 });
 
+
+/**
+ * userId: string
+  email: string
+  password: string
+  name: string
+  phone: string
+  address: string
+  dob: Date | null
+  userType: string
+  shippingAddress: string | null
+  billingAddress: string | null
+ */
 export type UserDtoType = Static<typeof UserDto>;
