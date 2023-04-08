b1b27233596658483bc846149e52adbd682697d2
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-04-08 17:41:54 +1000

    awesome

--- packages/backend/src/schema/auth.schema.ts
+++ packages/backend/src/schema/auth.schema.ts
@@ -1,30 +1,40 @@
-import { Static, Type } from '@sinclair/typebox'
+import { Static, Type } from "@sinclair/typebox";
 
-export const LoginSchema = Type.Object({
-  username: Type.String(),
-  password: Type.String(),
-}, {
-  description: 'LoginDto',
-  $id: 'LoginDto',
-  // $ref: 'LoginDto',
-})
+/**
+ * Login schema
+ * Used for data validation and documentation
+ */
+export const LoginSchema = Type.Object(
+  {
+    username: Type.String({ format: "email" }),
+    password: Type.String(),
+  },
+  {
+    description: "LoginDto",
+    $id: "LoginDto",
+    // $ref: 'LoginDto',
+  }
+);
 
-export const LoginSchemaRef = Type.Ref(LoginSchema)
+export const LoginSchemaRef = Type.Ref(LoginSchema);
 
-export type LoginSchemaType = Static<typeof LoginSchema>
+export type LoginSchemaType = Static<typeof LoginSchema>;
 
-export const RegisterSchema = Type.Object({
-    email: Type.String({ format: "email"}),
+export const RegisterSchema = Type.Object(
+  {
+    email: Type.String({ format: "email" }),
     password: Type.String(),
     name: Type.String(),
     phone: Type.String(),
     address: Type.String(),
-  }, {
-    description: 'RegisterDto',
-    $id: 'RegisterDto',
+  },
+  {
+    description: "RegisterDto",
+    $id: "RegisterDto",
     // $ref: 'RegisterDto',
-  })
-  
-  export const RegisterSchemaRef = Type.Ref(RegisterSchema)
-  
-  export type RegisterSchemaType = Static<typeof RegisterSchema>
+  }
+);
+
+export const RegisterSchemaRef = Type.Ref(RegisterSchema);
+
+export type RegisterSchemaType = Static<typeof RegisterSchema>;
