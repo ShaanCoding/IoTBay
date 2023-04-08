306d0bd972606edea0804da319e37f5981fe82dc
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-04-08 20:42:23 +1000

    devtools prod

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,21 +1,74 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { ButtonGroup, Container, Button } from "@chakra-ui/react";
-import { useNavigate } from "react-router-dom";
+import { RouterProvider, createBrowserRouter, useNavigate } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import DefaultLayout from "./layouts/Default";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import profileLoader from "./loaders/profileLoader";
+import Profile from "./pages/Profile";
+import Logout from "./pages/Logout";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient();
+
+const ReactQueryDevtoolsProduction = React.lazy(() =>
+  import('@tanstack/react-query-devtools/build/lib/index.prod.js').then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    }),
+  ),
+)
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DefaultLayout />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/profile",
+        loader: profileLoader(queryClient),
+        element: <Profile />,
+      },
+      {
+        path: "/logout",
+        element: <Logout />,
+      },
+    ],
+  },
+]);
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [showDevtools, setShowDevtools] = React.useState(false)
 
-  const navigate = useNavigate();
+  React.useEffect(() => {
+    // @ts-ignore
+    window.toggleDevtools = () => setShowDevtools((old) => !old)
+  }, [])
 
-  const navigateLogin = () => {
-    navigate("/login");
-  };
 
   return (
-    <Container>
-      <ButtonGroup>
-        <Button onClick={navigateLogin}>Login</Button>
-      </ButtonGroup>
-    </Container>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+      {showDevtools && (
+        <React.Suspense fallback={null}>
+          <ReactQueryDevtoolsProduction />
+        </React.Suspense>
+      )}
+    </QueryClientProvider>
   );
 }
--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,54 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import profileLoader from "./loaders/profileLoader";
-import Profile from "./pages/Profile";
-import DefaultLayout from "./layouts/Default";
-import Landing from "./pages/Landing";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Logout from "./pages/Logout";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+import App from "./App";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <DefaultLayout />,
-    children: [
-      {
-        index: true,
-        element: <Landing />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/register",
-        element: <Register />,
-      },
-      {
-        path: "/profile",
-        loader: profileLoader(queryClient),
-        element: <Profile />,
-      },
-      {
-        path: "/logout",
-        element: <Logout />,
-      },
-    ],
-  },
-]);
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ChakraProvider>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <App />
   </ChakraProvider>
 );
