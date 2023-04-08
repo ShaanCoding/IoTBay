0425505bfdfc6c80fcb6e3fa89dd34ce6cbd49d1
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-04-08 21:01:28 +1000

    better strat error handling

--- packages/backend/src/strategies/local.strategy.ts
+++ packages/backend/src/strategies/local.strategy.ts
@@ -6,17 +6,21 @@ import prisma from "../services/prisma.service";
  * Used to authenticate a user using a username and password.
  */
 export default new LocalStrategy(async (email, password, cb) => {
-  const user = await prisma.user.findUnique({ where: { email } });
-  if (!user) {
-    cb(undefined, false)
-    return;
-  }
-  const passwordValid = await argon2.verify(user.password, password);
-  if (!passwordValid) {
-    cb(undefined, false)
-    return;
-  }
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) {
+      cb(undefined, false);
+      return;
+    }
+    const passwordValid = await argon2.verify(user.password, password);
+    if (!passwordValid) {
+      cb(undefined, false);
+      return;
+    }
 
-  const { password: _password, ...rest} = user
-  cb(undefined, rest)
+    const { password: _password, ...rest } = user;
+    cb(undefined, rest);
+  } catch (error) {
+    cb(error, false);
+  }
 });
