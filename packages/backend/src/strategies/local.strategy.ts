8dcef239b66ae47f61b6bd0a2b0c878f5eebd40e
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-04-08 21:15:11 +1000

    remove old assets

--- packages/backend/src/strategies/local.strategy.ts
+++ packages/backend/src/strategies/local.strategy.ts
@@ -1,6 +1,10 @@
 import { Strategy as LocalStrategy } from "passport-local";
 import argon2 from "argon2";
 import prisma from "../services/prisma.service";
+import createHttpError from "http-errors";
+
+const invalidCredentials = createHttpError[400]("Invalid email or password");
+const unknownError = createHttpError[500]("Unknown error");
 
 /**
  * Used to authenticate a user using a username and password.
@@ -9,18 +13,18 @@ export default new LocalStrategy(async (email, password, cb) => {
   try {
     const user = await prisma.user.findUnique({ where: { email } });
     if (!user) {
-      cb(undefined, false);
+      cb(invalidCredentials, false);
       return;
     }
     const passwordValid = await argon2.verify(user.password, password);
     if (!passwordValid) {
-      cb(undefined, false);
+      cb(invalidCredentials, false);
       return;
     }
 
     const { password: _password, ...rest } = user;
     cb(undefined, rest);
   } catch (error) {
-    cb(error, false);
+    cb(unknownError, false);
   }
 });
