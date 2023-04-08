9d17f0a3df2378da91408aa3bc4cadcf72065d0d
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-04-08 15:49:44 +1000

    edits

--- packages/backend/src/controllers/auth.controller.ts
+++ packages/backend/src/controllers/auth.controller.ts
@@ -1,55 +1,61 @@
-import { FastifyRequest, FastifyReply } from 'fastify';
-import argon2 from 'argon2';
-import prisma from '../services/prisma.service';
-import { RegisterSchemaType } from '../schema';
+import { FastifyRequest, FastifyReply } from "fastify";
+import argon2 from "argon2";
+import prisma from "../services/prisma.service";
+import { LoginSchemaType, RegisterSchemaType } from "../schema";
 
-export const login = async (request: FastifyRequest, reply: FastifyReply) => {
-    const user = request.user;
+export const login = async (
+  request: FastifyRequest<{ Body: LoginSchemaType }>,
+  reply: FastifyReply
+) => {
+  const user = request.user;
 
-    if (!user) {
-      return reply.badRequest("Invalid credentials");
-    }
+  if (!user) {
+    return reply.badRequest("Invalid credentials");
+  }
 
-    return reply.status(200).send(user);
-}
+  return reply.status(200).send(user);
+};
 
 export const logout = async (request: FastifyRequest, reply: FastifyReply) => {
-    await request.logOut();
-    return reply.status(200).send({ message: "Logged out" });
-}
+  await request.logOut();
+  return reply.status(200).send({ message: "Logged out" });
+};
 
-export const register = async (request: FastifyRequest<{ Body: RegisterSchemaType }>, reply: FastifyReply) => {
-    const { email, password, name, phone, address } = request.body;
-    const passwordHash = await argon2.hash(password);
+export const register = async (
+  request: FastifyRequest<{ Body: RegisterSchemaType }>,
+  reply: FastifyReply
+) => {
+  const { email, password, name, phone, address } = request.body;
+  const passwordHash = await argon2.hash(password);
 
-    const userExists = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
+  const userExists = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
 
-    if (userExists) {
-      return reply.badRequest("User already exists");
-    }
+  if (userExists) {
+    return reply.badRequest("User already exists");
+  }
 
-    const user = await prisma.user.create({
-      data: {
-        email,
-        password: passwordHash,
-        name,
-        phone,
-        address,
-      },
-      select: {
-        userId: true,
-        email: true,
-        userType: true,
-        shippingAddress: true,
-        billingAddress: true,
-      },
-    });
+  const user = await prisma.user.create({
+    data: {
+      email,
+      password: passwordHash,
+      name,
+      phone,
+      address,
+    },
+    select: {
+      userId: true,
+      email: true,
+      userType: true,
+      shippingAddress: true,
+      billingAddress: true,
+    },
+  });
 
-    request.session.set("passport", user.userId);
+  request.session.set("passport", user.userId);
 
-    return reply.status(201).send(user);
-}
+  return reply.status(201).send(user);
+};
