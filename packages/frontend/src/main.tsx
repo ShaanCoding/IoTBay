58476769cd5cc33ebb314702792cd5445b205b89
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-04-08 11:11:57 +1000

    remove strict mode

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -8,11 +8,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+
     <ChakraProvider>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router} />
       </QueryClientProvider>
     </ChakraProvider>
-  </React.StrictMode>
 );
