7dba638d6e11909075f52db526b0de58d8237e65
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-05-14 09:48:13 +1000

    seed transactions

--- packages/backend/prisma/seed.ts
+++ packages/backend/prisma/seed.ts
@@ -3,37 +3,39 @@ const prisma = new PrismaClient();
 import argon2 from "argon2";
 
 async function main() {
-  await prisma.user.upsert({
-    where: { email: "staff@example.com" },
-    update: {},
-    create: {
-      email: "staff@example.com",
-      name: "Staff",
-      password: await argon2.hash("password"),
-      address: "123 Fake St",
-      phone: "1234567890",
-      userType: "staff",
-      staffDetails: {
-        create: {
-          position: "admin",
-          isActivated: true,
+  await prisma.$transaction([
+    prisma.user.upsert({
+      where: { email: "staff@example.com" },
+      update: {},
+      create: {
+        email: "staff@example.com",
+        name: "Staff",
+        password: await argon2.hash("password"),
+        address: "123 Fake St",
+        phone: "1234567890",
+        userType: "staff",
+        staffDetails: {
+          create: {
+            position: "admin",
+            isActivated: true,
+          },
         },
       },
-    },
-  });
+    }),
 
-  await prisma.user.upsert({
-    where: { email: "customer@example.com" },
-    update: {},
-    create: {
-      email: "customer@example.com",
-      name: "Customer",
-      password: await argon2.hash("password"),
-      address: "123 Fake St",
-      phone: "1234567890",
-      userType: "customer",
-    },
-  });
+    prisma.user.upsert({
+      where: { email: "customer@example.com" },
+      update: {},
+      create: {
+        email: "customer@example.com",
+        name: "Customer",
+        password: await argon2.hash("password"),
+        address: "123 Fake St",
+        phone: "1234567890",
+        userType: "customer",
+      },
+    }),
+  ]);
 }
 main()
   .then(async () => {
