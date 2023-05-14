e85f2ba84131b0dfc539c432d0b3b19875265e59
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-05-14 11:29:04 +1000

    seed other fields

--- packages/backend/prisma/seed.ts
+++ packages/backend/prisma/seed.ts
@@ -1,8 +1,60 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, User, Prisma } from "@prisma/client";
 const prisma = new PrismaClient();
 import argon2 from "argon2";
+import { faker } from "@faker-js/faker";
 
 async function main() {
+  const createFakeCustomerUser = (): Prisma.UserCreateInput => ({
+    email: faker.internet.email(),
+    address: faker.location.streetAddress(),
+    name: faker.internet.userName(),
+    password: faker.internet.password(),
+    phone: "1234567890",
+    userType: "customer",
+    customerDetails: {
+      create: {
+        sex: "irrelevant",
+      },
+    },
+  });
+
+  const createFakeStaffUser = (): Prisma.UserCreateInput => ({
+    email: faker.internet.email(),
+    address: faker.location.streetAddress(),
+    name: faker.internet.userName(),
+    password: faker.internet.password(),
+    phone: "1234567890",
+    userType: "staff",
+    staffDetails: {
+      create: {
+        position: faker.person.jobTitle(),
+        isActivated: true,
+      },
+    },
+  });
+
+  const createFakeProduct = (): Prisma.ProductCreateInput => ({
+    name: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    price: parseFloat(faker.commerce.price()),
+    stock: faker.number.int({ max: 100, min: 0 }),
+    category: faker.commerce.department(),
+    image: faker.image.url(),
+  });
+
+  const createFakeCategory = (): Prisma.ProductCategoryCreateInput => ({
+    name: faker.commerce.department(),
+  })
+
+  const fakeCustomers = faker.helpers.multiple(createFakeCustomerUser, { count: 20 });
+
+  const fakeStaff = faker.helpers.multiple(createFakeStaffUser, { count: 10 });
+
+  const fakeProducts = faker.helpers.multiple(createFakeProduct, { count: 10 });
+
+  const fakeCategories = faker.helpers.multiple(createFakeCategory, { count: 10 });
+
+
   await prisma.$transaction([
     prisma.user.upsert({
       where: { email: "staff@example.com" },
@@ -35,6 +87,35 @@ async function main() {
         userType: "customer",
       },
     }),
+
+    ...fakeCustomers.map((user) =>
+      prisma.user.upsert({
+        where: { email: user.email },
+        update: {},
+        create: user,
+      })
+    ),
+
+    ...fakeStaff.map((user) =>
+      prisma.user.upsert({
+        where: { email: user.email },
+        update: {},
+        create: user,
+      })
+    ),
+
+    ...fakeProducts.map((product) =>
+      prisma.product.create({
+        data: product,
+      })
+    ),
+
+    ...fakeCategories.map((category) =>
+      prisma.productCategory.create({
+        data: category,
+      })
+    ),
+
   ]);
 }
 main()
