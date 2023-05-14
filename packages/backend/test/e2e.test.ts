78c0c3b7d27ed6e8e67c00b162b77d70a7e19c4d
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-05-14 14:18:53 +1000

    e2e

--- packages/backend/test/e2e.test.ts
+++ packages/backend/test/e2e.test.ts
@@ -1,14 +1,38 @@
 import { expect, test, beforeAll, describe, afterEach, afterAll } from "vitest";
-import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import { CreateTRPCProxyClient, createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import type { AppRouter } from "../src";
 import { faker } from "@faker-js/faker";
 
 import fetchCookie from "fetch-cookie";
 import SuperJSON from "superjson";
 
+let trpcClient: CreateTRPCProxyClient<AppRouter>;
 
-describe("test", () => {
-  test("true is true", () => {
-    expect(true).toBe(true);
-  })
+beforeAll(async () => {
+  trpcClient = createTRPCProxyClient<AppRouter>({
+    links: [
+      httpBatchLink({
+        url: "http://127.0.0.1:3000/api/trpc",
+        fetch: fetchCookie(fetch),
+      }),
+    ],
+    transformer: SuperJSON,
+  });
 });
+
+const testUser = {
+  password: faker.internet.password(),
+  email: faker.internet.email(),
+  name: faker.internet.userName(),
+  phone: "1234567890",
+  address: faker.location.streetAddress(),
+};
+
+test("register a new customer", () => {
+  const registration = trpcClient.auth.register.mutate(testUser);
+  expect(registration).resolves.toBeDefined();
+})
+
+// add a product to cart
+
+// checkout
